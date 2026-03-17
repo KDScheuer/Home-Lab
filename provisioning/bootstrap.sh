6b960c910538984ec108bb6f6a972db38877faee
KDScheuer <kdscheuer97@gmail.com>
2026-03-16 22:05:31 -0600

    Downloading pip fro pypi in event it is not bundled in dnf install of python

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -9,7 +9,7 @@ sudo dnf update -y && sudo dnf upgrade -y
 echo "[+] Installing dependencies"
 sudo dnf install -y epel-release
 sudo dnf install -y git python3 python3-pip python3.11 tmux
-sudo python3.11 -m ensurepip --upgrade
+curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3.11
 python3.11 -m pip install --user --upgrade 'ansible-core>=2.16'
 export PATH=$HOME/.local/bin:$PATH
 grep -qxF 'export PATH=$HOME/.local/bin:$PATH' ~/.bashrc \
